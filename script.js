c9e956359ba42c4910d605d5e29dd08dddaddd8a
David Azevedo <davidazevedo1996@hotmail.com>
2018-04-17 10:26:35 +0100

    Finished scrapper

--- script.js
+++ script.js
@@ -1,28 +1,55 @@
 const axios = require('axios')
 const cheerio = require('cheerio')
+const fs = require('fs');
 
+// Base url for worten's website
 const baseUrl = 'https://www.worten.pt'
-const url = 'https://www.worten.pt/informatica/computadores/computadores-portateis?per_page=48&page=1'
-let items = []
+// URL for list of laptops
+const url = 'https://www.worten.pt/informatica/computadores/computadores-portateis?per_page=12&page=1'
+// URL example url for a detailed product page
+const testUrl = '/informatica/computadores/computadores-portateis/portatil-15-6-hp-15-bs109np-6435327'
 
+// Getting all product pages from the list
 axios.get(url).then((response) => {
     if (response.status === 200) {
         let $ = cheerio.load(response.data)
+        
         $('.w-product').each((i , elem) => {
-            console.log($(elem).attr('data-category'))
+            scrapProductDetails($(elem).attr('data-category'))
         })
     }
-});
+}).catch((error) => {
+    console.error(error)
+})
 
-/* TODO finish
+
+// Scraping each product detail page
 const scrapProductDetails = function(url) {
     axios.get(baseUrl + url).then((response) => {
         if (response.status === 200) {
             let $ = cheerio.load(response.data)
-            items.push({
-                $('.std').children('ul')
+            let wrapper = $('.w-section-content__box__wrapper')
+            
+            let obj = {
+                nome: wrapper.find('.w-product__name').text(),
+                'preço': wrapper.find('.w-product__price__current').attr('content'),
+                foto: baseUrl + wrapper.find('#product-main-image').attr('src')
+            }
+
+            let details = wrapper.find('.w-product-details__column')
+
+            details.find('.clearfix').each((i, elem) => {
+                obj[$(elem).find('.details-label').text()] = $(elem).find('.details-value').text()
             })
+
+            // Saving product information to a file
+            fs.appendFile('data.json', JSON.stringify(obj) + ',\n', (err) => {
+                if (err) 
+                    console.error(error)
+                console.log('Saved!')
+              })
         }
+    }).catch((error) => {
+        console.error(error)
     })
 }
-*/ 
