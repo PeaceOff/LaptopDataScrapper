f6a71da0abe6e8e9d902b98d79afb877b35df7de
David Azevedo <davidazevedo1996@hotmail.com>
2018-04-17 22:17:41 +0100

    Finished scrapper

--- script.js
+++ script.js
@@ -1,27 +1,41 @@
+require('dotenv').config()
+
 const axios = require('axios')
 const cheerio = require('cheerio')
 const fs = require('fs');
+const MongoClient = require('mongodb').MongoClient
+let dbo;
+
+MongoClient.connect(process.env.MONGO_URI, (err, db) => {
+    if(err) throw err
+
+    // Get db instance
+    dbo = db.db('db')
+
+    // Start scrapping
+    Start()
+})
+
 
 // Base url for worten's website
 const baseUrl = 'https://www.worten.pt'
-// URL for list of laptops
-const url = 'https://www.worten.pt/informatica/computadores/computadores-portateis?per_page=12&page=1'
-// URL example url for a detailed product page
-const testUrl = '/informatica/computadores/computadores-portateis/portatil-15-6-hp-15-bs109np-6435327'
 
 // Getting all product pages from the list
-axios.get(url).then((response) => {
-    if (response.status === 200) {
-        let $ = cheerio.load(response.data)
-        
-        $('.w-product').each((i , elem) => {
-            scrapProductDetails($(elem).attr('data-category'))
+const Start = function () {
+    for (i = 1; i < 6; i++) {
+        axios.get(`https://www.worten.pt/informatica/computadores/computadores-portateis?per_page=24&page=${i}`).then((response) => {
+            if (response.status === 200) {
+                let $ = cheerio.load(response.data)
+                
+                $('.w-product').each((i , elem) => {
+                    scrapProductDetails($(elem).attr('data-category'))
+                })
+            }
+        }).catch((error) => {
+            console.error(error)
         })
     }
-}).catch((error) => {
-    console.error(error)
-})
-
+}
 
 // Scraping each product detail page
 const scrapProductDetails = function(url) {
@@ -42,12 +56,19 @@ const scrapProductDetails = function(url) {
                 obj[$(elem).find('.details-label').text()] = $(elem).find('.details-value').text()
             })
 
+            // Send to DB instead
+            dbo.collection('Products').insertOne(obj, (err, res) => {
+                if (err) console.error(err)
+                console.log("OK")
+            })
             // Saving product information to a file
-            fs.appendFile('data.json', JSON.stringify(obj) + ',\n', (err) => {
+            /*
+            fs.appendFile('data.json', JSON.stringify(obj) + '\n', (err) => {
                 if (err) 
                     console.error(error)
                 console.log('Saved!')
               })
+              */
         }
     }).catch((error) => {
         console.error(error)
